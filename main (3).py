826517013f0ecb641855c80a4c735349dc121fee
Saurabh Sharma <115443864+saurabhsharma9@users.noreply.github.com>
2022-10-15 08:55:29 +0530

    Add files via upload

---
+++ main (3).py
@@ -0,0 +1,21 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age?")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+age_int = int(age)
+years_remaining = 90-age_int
+days_remaining = years_remaining *365
+weeks_reamaining = years_remaining*52
+months_reamaining = years_remaining*12
+message =(f"you have {days_remaining} days , {weeks_reamaining} weeks and {months_reamaining} months left")
+print(message)
+print("use your time well")
+
+
+
+
+
+
+
+
